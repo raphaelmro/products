ed1a4eaa336c7a3852dbab094dc81544b0033abb
Raphael Onofre <raphael.onofre@gmail.com>
2019-05-01 17:36:38 -0300

    Add axios, create local server(db.json) and make the categories dynamic

--- src/Produtos.js
+++ src/Produtos.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 import ProdutosHome from "./ProdutosHome";
-import Categoria from './Categoria'
+import Categoria from "./Categoria";
+import axios from "axios";
 
 class Produtos extends Component {
+  state = {
+    categorias: []
+  };
+
+  componentDidMount() {
+    axios
+        .get("http://localhost:3001/categorias")
+        .then(response => {
+            this.setState({
+            categorias: response.data
+        });
+    });
+  }
+
+  renderCategoria(categoria) {
+      return(
+          <li><Link key={categoria.id} to={`/produtos/categoria/${categoria.id}`}>
+              {categoria.categoria}
+          </Link></li>
+      )
+  }
+
   render() {
     const { match } = this.props;
+    const {categorias} = this.state
     return (
       <div className="row">
         <div className="col-md-2">
           <h3>Categorias</h3>
-          <Link to="/produtos/categoria/1">Categoria 1</Link>
+            <ul>
+            {categorias.map(this.renderCategoria)}
+            </ul>
         </div>
         <div className="col-md-10">
           <h1>Produtos</h1>
