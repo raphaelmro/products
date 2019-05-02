e8cacd6570f57ca75618058b402f524cb0100b57
Raphael Onofre <raphael.onofre@gmail.com>
2019-05-02 01:59:53 -0300

    feat: add categoria

--- src/Categoria.js
+++ src/Categoria.js
@@ -1,13 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Categoria extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Categoria {this.props.match.params.catId}</h1>
-            </div>
-        );
-    }
+  state = {
+    produtos: [],
+    categoria: {}
+  };
+
+  loadData(id) {
+    axios
+      .get("http://localhost:3001/produtos?categoria=" + id)
+      .then(response => {
+        this.setState({
+          produtos: response.data
+        });
+      });
+    axios
+        .get("http://localhost:3001/categorias/" + id)
+        .then(response => {
+            this.setState({
+            categoria: response.data
+        });
+    });
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.catId;
+    this.loadData(id);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.loadData(newProps.match.params.catId);
+  }
+
+  renderProduto(produto) {
+    return (
+      <p className="well" key={produto.id}>
+        {produto.produto}
+      </p>
+    );
+  }
+
+  render() {
+    const { produtos } = this.state;
+    return (
+      <div>
+          <h1>{this.state.categoria.categoria}</h1>
+        {produtos.map(this.renderProduto)}
+      </div>
+    );
+  }
 }
 
 export default Categoria;
--- src/Produtos.js
+++ src/Produtos.js
@@ -9,34 +9,54 @@ class Produtos extends Component {
     categorias: []
   };
 
-  componentDidMount() {
-    axios
-        .get("http://localhost:3001/categorias")
-        .then(response => {
-            this.setState({
-            categorias: response.data
-        });
+  loadCategorias = () => {
+    axios.get("http://localhost:3001/categorias").then(response => {
+      this.setState({
+        categorias: response.data
+      });
     });
+  };
+  componentDidMount() {
+    this.loadCategorias();
   }
 
-  renderCategoria(categoria) {
-      return(
-          <li><Link key={categoria.id} to={`/produtos/categoria/${categoria.id}`}>
-              {categoria.categoria}
-          </Link></li>
-      )
+  renderCategoria(cat) {
+    return (
+      <li key={cat.id}>
+        <Link to={`/produtos/categoria/${cat.id}`}>{cat.categoria}</Link>
+      </li>
+    );
   }
+
+  handleNewCategoria = key => {
+    if (key.keyCode === 13) {
+      axios
+        .post("http://localhost:3001/categorias", {
+          categoria: this.refs.categoria.value
+        })
+        .then(response => {
+          this.refs.categoria.value = "";
+          this.loadCategorias();
+        });
+    }
+  };
 
   render() {
     const { match } = this.props;
-    const {categorias} = this.state
+    const { categorias } = this.state;
     return (
       <div className="row">
         <div className="col-md-2">
           <h3>Categorias</h3>
-            <ul>
-            {categorias.map(this.renderCategoria)}
-            </ul>
+          <ul>{categorias.map(this.renderCategoria)}</ul>
+          <div className="well">
+            <input
+              type="text"
+              onKeyUp={this.handleNewCategoria}
+              ref="categoria"
+              placeholder="Nova Categoria"
+            />
+          </div>
         </div>
         <div className="col-md-10">
           <h1>Produtos</h1>
